14b35c48d9ddbb095d57adbb62e9ec9226ec2eb9
MZen2610 <mzen1980@gmail.com>
2021-02-04 21:36:59 +0300

    added models

--- project/modelsdata/admin.py
+++ project/modelsdata/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Predictionmodel, Predictionrequest
+from django.utils.html import format_html
 
-# Register your models here.
+class PredictionmodelInline(admin.TabularInline):
+    model = Predictionmodel
+
+    readonly_fields = ("get_preview",)
+
+    def get_preview(self, obj):
+        return format_html('<img src="{url}" width="{width}" height={height} />'.format(
+            url=obj.image.url,
+            width=obj.image.width if obj.image.width < 200 else 200,
+            height=obj.image.height if obj.image.height < 200 else 200,
+        )
+        )
+
+    fields = ('title', 'img', 'get_preview', 'free', 'slug',)
+
+
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [
+        PredictionmodelInline,
+    ]
+
+    fields = ('slug', 'title', 'number', 'addres_url',)
+
+    class Meta:
+        model = Predictionrequest
+
+
+admin.site.register(Predictionmodel)
+admin.site.register(Predictionrequest)
+
+
--- project/modelsdata/models.py
+++ project/modelsdata/models.py
@@ -1,15 +1,40 @@
 from django.db import models
+from django.urls import reverse
 
-class Addressbar(models.Model):
-    slug = models.SlugField()  # Для создания своего URL адреса
-    title = models.CharField(max_length=120, verbose_name='Наименование')
-    addres = models.URLField(max_length=250, verbose_name='url')
+class Predictionmodel(models.Model):
+    slug = models.SlugField() # Для создания своего URL адреса
+    title = models.CharField(max_length=120)
     comment = models.TextField(blank=True, verbose_name='Комментарий')
+    img = models.ImageField(default='default.png', upload_to='model_images', blank=True, verbose_name='Изображение',
+                            null=True)
+    free = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('model-detail', kwargs={'slug': self.slug})
+
     class Meta:
-        verbose_name = 'Адрес'
-        verbose_name_plural = 'Адреса'
+        verbose_name = 'Модель'
+        verbose_name_plural = 'Модели'
+        ordering = ['title']
+
+class Predictionrequest(models.Model):
+    slug = models.SlugField()  # Для создания своего URL адреса
+    title = models.CharField(max_length=120, verbose_name='Наименование')
+    comment = models.TextField(blank=True, verbose_name='Комментарий')
+    predictionmodel = models.ForeignKey(Predictionmodel, on_delete=models.SET_NULL, null=True)
+    number = models.IntegerField()
+    addres_url = models.URLField(max_length=250, verbose_name='url-адрес', blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('lesson-detail', kwargs={'slug': self.predictionmodel.slug, 'lesson_slug': self.slug})
+
+    class Meta:
+        verbose_name = 'Запрос'
+        verbose_name_plural = 'Запросы'
         ordering = ['title']
