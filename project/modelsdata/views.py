3c80b878362b82d702b46804258ffe70f1779fbc
MZen2610 <mzen1980@gmail.com>
2021-02-06 09:10:20 +0300

    Добавил главную страницу

--- project/modelsdata/views.py
+++ project/modelsdata/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from .models import *
+from django.views.generic import ListView
 
-# Create your views here.
+class HomePage(ListView):
+		model = Predictionmodel
+		template_name = 'modelsdata/home.html'
+		context_object_name = 'modelsdata'
+		ordering = ['-id']
+
+		def get_context_data(self, *, object_list=None, **kwargs):
+			ctx = super(HomePage, self).get_context_data(**kwargs)
+			ctx['title'] = 'Главная страница сайта'
+			return ctx
+
